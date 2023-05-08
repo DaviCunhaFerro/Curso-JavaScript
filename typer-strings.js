6b5d16708761f6331ca6b8d4e69efd3adaf4df39
Elton Montilha <elton.vmontilha@gmai.com>
2023-05-08 16:34:20 -0300

    exemplos da aula 1

---
+++ typer-strings.js
@@ -0,0 +1,28 @@
+const texto1 = "Olá Mundo!";
+const senha= "SenhaSuperSecreta123";
+const Numeros = "123456789";
+
+const citacao = "Meu Vero amigo e ";
+const MeuVeroAmigo = "Jesus";
+
+
+// concatenaçao (+)
+
+
+
+
+
+//const cifrao = '\u0024'
+//const aMaiusculo = '\u0041'
+//const tique = '\u2705'
+//const hiragana = '\u3041'
+
+//console.log(cifrao)
+//console.log(aMaiusculo)
+//console.log(tique)
+//console.log(hiragana)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
